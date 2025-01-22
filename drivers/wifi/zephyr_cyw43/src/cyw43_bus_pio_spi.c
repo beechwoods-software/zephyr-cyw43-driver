ecfdded877ddbb4fb24bb70d182e6e615e8f015c
Dave Rensberger <davidr@beechwoods.com>
2025-01-22 10:28:27 -0500

    I think this works now

--- drivers/wifi/zephyr_cyw43/src/cyw43_bus_pio_spi.c
+++ drivers/wifi/zephyr_cyw43/src/cyw43_bus_pio_spi.c
@@ -16,6 +16,7 @@
 #include "cyw43_spi.h"
 #include "cyw43_debug_pins.h"
 
+#include <zephyr/drivers/pinctrl.h>
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/drivers/spi.h>
 
@@ -24,19 +25,6 @@
 #define DT_DRV_COMPAT infineon_cyw43
 
 #if CYW43_SPI_PIO
-#define WL_REG_ON CYW43_PIN_WL_REG_ON
-#define DATA_OUT_PIN CYW43_PIN_WL_CMD
-#define DATA_IN_PIN CYW43_PIN_WL_CMD
-#define IRQ_PIN CYW43_PIN_WL_CMD
-
-#define CLOCK_PIN CYW43_PIN_WL_CLOCK
-#define CS_PIN CYW43_PIN_WL_CS
-
-#define IRQ_SAMPLE_DELAY_NS 100
-
-#define CLOCK_DIV 2
-#define CLOCK_DIV_MINOR 0
-#define PADS_DRIVE_STRENGTH PADS_BANK0_GPIO0_DRIVE_VALUE_12MA
 
 #if !CYW43_USE_SPI
 #error CYW43_USE_SPI should be true
@@ -56,141 +44,12 @@ static uint32_t counter = 0;
 #define DUMP_SPI_TRANSACTIONS(A)
 #endif
 
+#undef SWAP32
 __force_inline static uint32_t __swap16x2(uint32_t a) {
         __asm ("rev16 %0, %0" : "+l" (a) : : );
         return a;
 }
 #define SWAP32(a) __swap16x2(a)
-
-struct cyw43_pio_spi_config {
-        struct spi_dt_spec bus;
-};
-
-struct cyw43_pio_spi_data {
-        const struct cyw43_pio_spi_config *cfg;
-};
-
-static const struct cyw43_pio_spi_config cyw43_config_pio_spi0 = {
-        .bus = SPI_DT_SPEC_INST_GET(0, SPI_OP_MODE_MASTER | SPI_TRANSFER_MSB |
-                                    SPI_WORD_SET(8) | SPI_HOLD_ON_CS |
-                                    SPI_LOCK_ON, 1000U),
-};
-
-static struct cyw43_pio_spi_data cyw43_pio_spi0;
-
-static const struct device *gpio = DEVICE_DT_GET(DT_NODELABEL(gpio0));
-
-#define SD_ON_PIN       23
-#define SD_CMD_PIN      24
-#define SD_DIN_PIN      24
-#define SD_D0_PIN       24
-#define SD_CS_PIN       25
-#define SD_CLK_PIN      29
-#define SD_IRQ_PIN      24
-#define SD_LED_GPIO     0
-
-/* I/O pin configuration */
-#define IO_IN         0
-#define IO_OUT        1
-#define IO_NOPULL     0
-#define IO_PULLDN     1
-#define IO_PULLUP     2
-
-
-/* Set input or output */
-void io_mode(int pin, int mode)
-{
-    gpio_set_dir(pin, mode==IO_OUT);
-}
-
-/* Set I/P pullup or pulldown */
-void io_pull(int pin, int pull)
-{
-        if (pull == IO_PULLUP) {
-                gpio_pull_up(pin);
-        }
-        else if (pull == IO_PULLDN) {
-                gpio_pull_down(pin);
-        }
-        else {
-                gpio_disable_pulls(pin);
-        }
-}
-
-void io_init(void)
-{
-        printf("calling gpio_set_input_hysteresis_enabled(SD_DIN_PIN, true); \n");
-        gpio_set_input_hysteresis_enabled(SD_DIN_PIN, true);
-}
-
-/* Set input or output with pullups */
-void io_set(int pin, int mode, int pull)
-{
-        gpio_init(pin);
-        io_mode(pin, mode);
-        io_pull(pin, pull);
-}
-
-/* Set an O/P pin */
-static inline void io_out(int pin, int val)
-{
-        gpio_put(pin, val != 0);
-}
-
-/* Get an I/P pin value */
-static inline uint8_t io_in(int pin)
-{
-        return(gpio_get(pin));
-}
-
-void picowi_gpio_setup() {
-        printf("picowi_gpio_setup()\n");
-        io_set(SD_ON_PIN, IO_OUT, IO_NOPULL);
-        io_out(SD_ON_PIN, 0);
-        io_set(SD_CS_PIN, IO_OUT, IO_NOPULL);
-        io_out(SD_CS_PIN, 1);
-        io_set(SD_CLK_PIN, IO_OUT, IO_NOPULL);
-        io_out(SD_CLK_PIN, 0);
-        io_set(SD_CMD_PIN, IO_OUT, IO_NOPULL);
-        io_out(SD_CMD_PIN, 0);
-        k_busy_wait(100000);
-        io_out(SD_ON_PIN, 1);
-        k_busy_wait(50000);
-        io_set(SD_CMD_PIN, IO_IN, IO_PULLUP);
-        io_set(SD_IRQ_PIN, IO_IN, IO_NOPULL);
-}
-
-int cyw43_spi_init(cyw43_int_t *self) {
-
-        struct cyw43_pio_spi_data *spi = &cyw43_pio_spi0; /* Static instance */
-        const struct cyw43_pio_spi_config *cfg = &cyw43_config_pio_spi0; /* Static instance */
-        printf("\n\nRunning with the Zephyr PIO-SPI low level interface\n\n\n");
-
-        gpio_set_input_hysteresis_enabled(24, true);
-
-        k_busy_wait(2000);
-
-        picowi_gpio_setup();
-
-        struct gpio_dt_spec irq_gpio = GPIO_DT_SPEC_GET(DT_NODELABEL(infineon_cyw43_module), host_wake_gpios);
-
-        gpio_pin_interrupt_configure_dt(&irq_gpio, GPIO_INT_DISABLE);
-
-        /* Only does something if CYW43_LOGIC_DEBUG=1 */
-        logic_debug_init();
-
-        spi->cfg = cfg;
-        assert(!self->bus_data);
-        self->bus_data = spi;
-
-        cyw43_spi_reset();
-
-        return 0;
-}
-
-void cyw43_spi_deinit(cyw43_int_t *self) {
-        return;
-}
 
 #if ENABLE_SPI_DUMPING
 static void dump_bytes(const uint8_t *bptr, uint32_t len) {
@@ -206,95 +65,209 @@ static void dump_bytes(const uint8_t *bptr, uint32_t len) {
 }
 #endif
 
+#define WHD_BUS_SPI_BACKPLANE_READ_PADD_SIZE        (4)
+
+
+struct cyw43_pio_spi_data {
+        const struct cyw43_wifi_config *cfg;
+};
+
+#define PINCTRL_STATE_HOST_WAKE PINCTRL_STATE_PRIV_START
+PINCTRL_DT_INST_DEFINE(0);
+
+#define WLAN_CBUCK_DISCHARGE_MS 10
+#define WLAN_POWER_UP_DELAY_MS 250
+
+#define CYW43_WIFI_SPI_OPERATION (SPI_WORD_SET(DT_PROP_OR(DT_DRV_INST(0), spi_word_size, 8)) \
+                                  | SPI_HALF_DUPLEX \
+                                  | SPI_TRANSFER_MSB)
+
+struct cyw43_wifi_config cyw43_wifi_config = {
+        .bus_spi = SPI_DT_SPEC_GET(DT_DRV_INST(0), CYW43_WIFI_SPI_OPERATION, 0),
+
+        .wl_on_gpio = GPIO_DT_SPEC_GET_OR(DT_DRV_INST(0), wl_on_gpios, {0}),
+        .host_wake_gpio = GPIO_DT_SPEC_GET(DT_NODELABEL(infineon_cyw43_module), host_wake_gpios),
+        .bus_select_gpio = GPIO_DT_SPEC_GET_OR(DT_DRV_INST(0), bus_select_gpios, {0}),
+
+        .pcfg = PINCTRL_DT_INST_DEV_CONFIG_GET(0),
+};
+
+static struct cyw43_pio_spi_data cyw43_pio_spi0;
+
+int cyw43_spi_init(cyw43_int_t *self) {
+
+        struct cyw43_pio_spi_data *spi = &cyw43_pio_spi0; /* Static instance */
+        struct cyw43_wifi_config *config = &cyw43_wifi_config; /* Static instance */
+        
+        printf("\n\nRunning with the Zephyr PIO-SPI low level interface\n\n\n");
+
+        int ret;
+
+	/* Pull bus select line low before enabling WiFi chip */
+	gpio_pin_configure_dt(&config->bus_select_gpio, GPIO_OUTPUT_INACTIVE);
+
+        /* power wifi on */
+        if (!device_is_ready(config->wl_on_gpio.port)) {
+            printk("Error: failed to configure wifi_reg_on %s pin %d\n",
+                   config->wl_on_gpio.port->name, config->wl_on_gpio.pin);
+            return -1;
+        }
+
+        ret = gpio_pin_configure_dt(&config->wl_on_gpio, GPIO_OUTPUT);
+        if (ret) {
+            printk("Error %d: failed to configure wifi_reg_on %s pin %d\n", ret,
+                   config->wl_on_gpio.port->name, config->wl_on_gpio.pin);
+            return ret;
+        }
+
+        ret = gpio_pin_set_dt(&config->wl_on_gpio, 0);
+	if (ret) {
+            printk("gpio_pin_set_dt(&config->wl_on_gpio, 0) returned %d\n", ret);
+            return ret;
+	}
+
+	/* Allow CBUCK regulator to discharge */
+	k_msleep(WLAN_CBUCK_DISCHARGE_MS);
+
+	/* WIFI power on */
+	ret = gpio_pin_set_dt(&config->wl_on_gpio, 1);
+	if (ret) {
+            printk("gpio_pin_set_dt(&config->wl_on_gpio, 1) returned %d\n", ret);
+            return ret;
+	}
+	k_msleep(WLAN_POWER_UP_DELAY_MS);
+        
+
+        if (!spi_is_ready_dt(&config->bus_spi)) {
+		printk("SPI device is not ready\n");
+		return -1;
+	}
+        
+        cyw43_spi_irq_enable(config, true);
+
+
+        /* Only does something if CYW43_LOGIC_DEBUG=1 */
+        logic_debug_init();
+
+        spi->cfg = config;
+        assert(!self->bus_data);
+        self->bus_data = spi;
+
+        return 0;
+}
+
+void cyw43_spi_deinit(cyw43_int_t *self) {
+        return;
+}
+
 int cyw43_spi_transfer(cyw43_int_t *self, const uint8_t *tx, size_t tx_length, uint8_t *rx,
                        size_t rx_length) {
         int rv = -CYW43_EINVAL;
         struct cyw43_pio_spi_data *bus_data = (struct cyw43_pio_spi_data *)self->bus_data;
-        struct spi_buf spi_tx_buf;
-        const struct spi_buf_set spi_tx = {
-                .buffers = &spi_tx_buf,
-                .count = 1
-        };
 
+        struct cyw43_wifi_config *cfg = &cyw43_wifi_config;
+
+        /*
         CYW43_VDEBUG("Calling cyw43_spi_transfer(self=0x%lx, tx=0x%lx, tx_length=%d, rx=0x%lx, rx_length=%d)\n",
                      (unsigned long)self, (unsigned long)tx, (unsigned int)tx_length,
                      (unsigned long)rx, (unsigned int)rx_length);
-
-        io_out(CS_PIN, 0);
+        */
+        
+        cyw43_spi_irq_enable(cfg, false);
 
         if ((tx == NULL) && (rx == NULL)) {
                 return CYW43_FAIL_FAST_CHECK(-CYW43_EINVAL);
         }
 
-        if (rx != NULL) {
-                struct spi_buf spi_rx_buf = {
-                        .buf = rx + tx_length,
-                        .len = rx_length - tx_length
-                };
-                struct spi_buf_set spi_rx = {
-                        .buffers = &spi_rx_buf,
-                        .count = 1
-                };
+	if (tx == NULL && tx_length > 0 && rx_length >= tx_length) {
+		tx = rx;
+	}
+    
 
-                if (tx == NULL) {
-                        tx = rx;
-                }
+	const struct spi_buf tx_buf = {.buf = (uint8_t *)tx, .len = tx_length};
+	const struct spi_buf_set tx_set = {.buffers = &tx_buf, .count = 1};
+	struct spi_buf rx_buf[2];
+        struct spi_buf_set rx_set = {.buffers = rx_buf, .count = 2};
 
-                DUMP_SPI_TRANSACTIONS(
-                        printf("[%u] bus TX/RX %u bytes rx %u:", counter++, tx_length, rx_length);
-                        dump_bytes(tx, tx_length);
-                )
+	if (rx != NULL) {
+		rx += tx_length;
+	}
+        
+	if (rx_length >= tx_length) {
+		rx_length -= tx_length;
+	} else {
+		rx_length = 0;
+	}
+        
+        rx_buf[0].buf = rx;
+        rx_buf[0].len = rx_length;
+        rx_set.count = 1;
+        
+        /*
+        CYW43_VDEBUG("Calling spi_transceive_dt(0x%lx, rx_buf.len=%d, tx_buf.len=%d)\n",
+                     (long unsigned) &bus_data->cfg->bus_spi, tx_set.buffers[0].len, rx_set.buffers[0].len);
+        */
+        DUMP_SPI_TRANSACTIONS(
+                printf("TXed:");
+                dump_bytes(tx, tx_length);
+                printf("\n");
+        )
 
-                spi_tx_buf.buf = (void *)tx;
-                spi_tx_buf.len = tx_length;
-
-                rv = spi_transceive_dt(&bus_data->cfg->bus, &spi_tx, &spi_rx);
-                CYW43_VDEBUG("spi_transceive_dt() returned %d\n", rv);
-
-                DUMP_SPI_TRANSACTIONS(
-                        printf("RXed:");
-                        dump_bytes(rx, rx_length);
-                        printf("\n");
-                )
-
-                spi_release_dt(&bus_data->cfg->bus);
-        } else if (tx != NULL) {
-                DUMP_SPI_TRANSACTIONS(
-                        printf("[%u] bus TX only %u bytes:", counter++, tx_length);
-                        dump_bytes(tx, tx_length);
-                )
-
-                spi_tx_buf.buf = (void *)tx;
-                spi_tx_buf.len = tx_length;
-
-                rv = spi_transceive_dt(&bus_data->cfg->bus, &spi_tx, NULL);
-                CYW43_VDEBUG("spi_transceive_dt() returned %d\n", rv);
-
-                spi_release_dt(&bus_data->cfg->bus);
+	rv = spi_transceive_dt(&bus_data->cfg->bus_spi, &tx_set, &rx_set);
+        if (rv != 0) {
+            printk("rv = %d\n", rv);
+            CYW43_VDEBUG("rv = %d\n", rv);
         }
 
-        io_out(SD_CS_PIN, 1);
+        DUMP_SPI_TRANSACTIONS(
+                printf("RXed:");
+                dump_bytes(rx, rx_length);
+                printf("\n");
+        )
 
+        cyw43_spi_irq_enable(cfg, true);
+        
+        //CYW43_VDEBUG("spi_transceive_dt() returned %d\n", rv);
+        
         return rv;
+}
+
+
+void cyw43_spi_irq_enable(struct cyw43_wifi_config *cfg, bool enable) {
+    int ret;
+    //printk("cyw43_spi_irq_enable(%s)\n", enable ? "true" : "false");
+
+    if (enable) {
+        ret = pinctrl_apply_state(cyw43_wifi_config.pcfg, PINCTRL_STATE_HOST_WAKE);
+        if (ret) {
+            printk("could not apply pinconfig HOST_WAKE, return code %d\n", ret);
+        }
+
+        ret = gpio_pin_interrupt_configure_dt(&cyw43_wifi_config.host_wake_gpio, GPIO_INT_EDGE_FALLING);
+        if (ret) {
+                printk("Unable to configure GPIO pin %u\n", cyw43_wifi_config.host_wake_gpio.pin);
+        }
+        gpio_pin_interrupt_configure_dt(&cfg->host_wake_gpio, (GPIO_INT_ENABLE|GPIO_INT_LEVEL_HIGH));
+
+    } else {
+        gpio_pin_interrupt_configure_dt(&cfg->host_wake_gpio, GPIO_INT_DISABLE);
+        ret = pinctrl_apply_state(cyw43_wifi_config.pcfg, PINCTRL_STATE_DEFAULT);
+        if (ret) {
+            printk("could not apply pinconfig DEFAULT, return code %d\n", ret);
+        }        
+    }
+ 
+    return;
 }
 
 /* Initialise our gpios */
 void cyw43_spi_gpio_setup(void) {
-        /* this function would normally set up GPIO pins (direction, etc).
-           since this is already done as dictated in the dts by Zephyr
-           the function is a NOP for our port.
-        */
-        gpio_init(WL_REG_ON);
-        gpio_set_dir(WL_REG_ON, GPIO_OUT);
         return;
 }
 
 /* Reset wifi chip */
 void cyw43_spi_reset(void) {
-        gpio_pin_set(gpio, WL_REG_ON, false); /* off */
-        k_sleep(K_MSEC(20));
-        gpio_pin_set(gpio, WL_REG_ON, true); /* on */
-        k_sleep(K_MSEC(250));
+        return;
 }
 
 static inline uint32_t make_cmd(bool write, bool inc, uint32_t fn, uint32_t addr, uint32_t sz) {
@@ -319,6 +292,7 @@ static const char *func_name(int fn) {
 
 uint32_t read_reg_u32_swap(cyw43_int_t *self, uint32_t fn, uint32_t reg) {
         uint32_t buf[2] = {0};
+        //CYW43_VDEBUG("Calling read_reg_u32_swap()");        
         assert(fn != BACKPLANE_FUNCTION);
         /*  DEBUG */
         /*  buf[0] = SWAP32(make_cmd(false, true, fn, reg, 4)); */
@@ -376,6 +350,7 @@ int cyw43_read_reg_u8(cyw43_int_t *self, uint32_t fn, uint32_t reg) {
 
 /* This is only used to switch the word order on boot */
 int write_reg_u32_swap(cyw43_int_t *self, uint32_t fn, uint32_t reg, uint32_t val) {
+    //CYW43_VDEBUG("Calling write_reg_u32_swap()");
         uint32_t buf[2];
         /* Boots up in little endian so command needs swapping too */
         buf[0] = SWAP32(make_cmd(true, true, fn, reg, 4));
@@ -387,6 +362,7 @@ int write_reg_u32_swap(cyw43_int_t *self, uint32_t fn, uint32_t reg, uint32_t va
 }
 
 static inline int _cyw43_write_reg(cyw43_int_t *self, uint32_t fn, uint32_t reg, uint32_t val, uint size) {
+    //CYW43_VDEBUG("Calling  _cyw43_write_reg()");    
         uint32_t buf[2];
         buf[0] = make_cmd(true, true, fn, reg, size);
         buf[1] = val;
@@ -425,21 +401,26 @@ int cyw43_write_reg_u8(cyw43_int_t *self, uint32_t fn, uint32_t reg, uint32_t va
         return _cyw43_write_reg(self, fn, reg, val, 1);
 }
 
-#if MAX_BLOCK_SIZE > 0x7f8
+#if CYW43_BUS_MAX_BLOCK_SIZE > 0x7f8
 #error Block size is wrong for SPI
 #endif
 
 int cyw43_read_bytes(cyw43_int_t *self, uint32_t fn, uint32_t addr, size_t len, uint8_t *buf) {
-        assert(fn != BACKPLANE_FUNCTION || (len <= 64 && (addr + len) <= 0x8000));
-        const uint32_t padding = (fn == BACKPLANE_FUNCTION) ? 4 : 0; // Add response delay
+
+    CYW43_VDEBUG("Calling cyw43_read_bytes()"); 
+        /* this flag is initialized to true, but should remain false starting with the first call 
+           to this function. */
+
+        assert(fn != BACKPLANE_FUNCTION || (len <= CYW43_BUS_MAX_BLOCK_SIZE));
+        const uint32_t padding = (fn == BACKPLANE_FUNCTION) ? CYW43_BACKPLANE_READ_PAD_LEN_BYTES : 0; // Add response delay
         size_t aligned_len = (len + 3) & ~3;
         assert(aligned_len > 0 && aligned_len <= 0x7f8);
         assert(buf == self->spid_buf || buf < self->spid_buf || buf >= (self->spid_buf + sizeof(self->spid_buf)));
-        self->spi_header[padding > 0 ? 0 : 1] = make_cmd(false, true, fn, addr, len + padding);
+        self->spi_header[padding > 0 ? 0 : (CYW43_BACKPLANE_READ_PAD_LEN_BYTES / 4)] = make_cmd(false, true, fn, addr, len);
         if (fn == WLAN_FUNCTION) {
                 logic_debug_set(pin_WIFI_RX, 1);
         }
-        int ret = cyw43_spi_transfer(self, NULL, 4, (uint8_t *)&self->spi_header[padding > 0 ? 0 : 1], aligned_len + 4 + padding);
+        int ret = cyw43_spi_transfer(self, NULL, 4, (uint8_t *)&self->spi_header[padding > 0 ? 0 : (CYW43_BACKPLANE_READ_PAD_LEN_BYTES / 4)], aligned_len + 4 + padding);
         if (fn == WLAN_FUNCTION) {
                 logic_debug_set(pin_WIFI_RX, 0);
         }
@@ -447,9 +428,9 @@ int cyw43_read_bytes(cyw43_int_t *self, uint32_t fn, uint32_t addr, size_t len, 
                 printf("cyw43_read_bytes error %d", ret);
                 return ret;
         }
-    if (buf != self->spid_buf) { // avoid a copy in the usual case just to add the header
-        memcpy(buf, self->spid_buf, len);
-    }
+        if (buf != self->spid_buf) { // avoid a copy in the usual case just to add the header
+            memcpy(buf, self->spid_buf, len);
+        }
     return 0;
 }
 
@@ -459,7 +440,7 @@ int cyw43_read_bytes(cyw43_int_t *self, uint32_t fn, uint32_t addr, size_t len, 
 */
 int cyw43_write_bytes(cyw43_int_t *self, uint32_t fn, uint32_t addr, size_t len, const uint8_t *src) {
         assert(fn != BACKPLANE_FUNCTION || (len <= CYW43_BUS_MAX_BLOCK_SIZE));
-        size_t aligned_len = (len + 3) & ~3u;
+        const size_t aligned_len = (len + 3) & ~3u;
         assert(aligned_len > 0 && aligned_len <= 0x7f8);
         if (fn == WLAN_FUNCTION) {
                 /* Wait for FIFO to be ready to accept data */
@@ -479,17 +460,17 @@ int cyw43_write_bytes(cyw43_int_t *self, uint32_t fn, uint32_t addr, size_t len,
                 }
         }
         if (src == self->spid_buf) { // avoid a copy in the usual case just to add the header
-                self->spi_header[1] = make_cmd(true, true, fn, addr, len);
+                self->spi_header[(CYW43_BACKPLANE_READ_PAD_LEN_BYTES / 4)] = make_cmd(true, true, fn, addr, len);
                 logic_debug_set(pin_WIFI_TX, 1);
-                int res = cyw43_spi_transfer(self, (uint8_t *)&self->spi_header[1], aligned_len + 4, NULL, 0);
+                int res = cyw43_spi_transfer(self, (uint8_t *)&self->spi_header[(CYW43_BACKPLANE_READ_PAD_LEN_BYTES / 4)], aligned_len + 4, NULL, 0);	
                 logic_debug_set(pin_WIFI_TX, 0);
                 return res;
         } else {
                 /* todo: would be nice to get rid of this. Only used for firmware download? */
                 assert(src < self->spid_buf || src >= (self->spid_buf + sizeof(self->spid_buf)));
-                self->spi_header[1] = make_cmd(true, true, fn, addr, len);
+                self->spi_header[(CYW43_BACKPLANE_READ_PAD_LEN_BYTES / 4)] = make_cmd(true, true, fn, addr, len);	
                 memcpy(self->spid_buf, src, len);
-                return cyw43_spi_transfer(self, (uint8_t *)&self->spi_header[1], aligned_len + 4, NULL, 0);
-    }
+                return cyw43_spi_transfer(self, (uint8_t *)&self->spi_header[(CYW43_BACKPLANE_READ_PAD_LEN_BYTES / 4)], aligned_len + 4, NULL, 0);
+        }
 }
 #endif
